5397f6dc595fc519b41fb7b89827ac7495712a07
Martina <martinarahn18@gmail.com>
2026-06-04 14:43:46 +0100

    Updated suite2p settings for new nested API struct

--- preprocessing/suite2p/run_suite2p.py
+++ preprocessing/suite2p/run_suite2p.py
@@ -4,6 +4,11 @@ Adapted from https://github.com/MouseLand/suite2p/blob/main/jupyter/Run%20Suite2
 import argparse
 import os
 import suite2p
+from pathlib import Path
+import sys
+
+print("First sys.path entry:", sys.path[0])
+print("Using suite2p from:", suite2p.__file__)
 
 parser = argparse.ArgumentParser(description="Run Suite2p with animal and session inputs.")
 parser.add_argument('--animal', type=str, default='TAA0000061', help="The animal ID (e.g. 'TAA0000066')")
@@ -11,37 +16,54 @@ parser.add_argument('--session', type=str, default='ses-007_date-20250307_protoc
 args = parser.parse_args()
 
 # Data directory
-# basepath = "/Volumes/mrsic_flogel/public/projects/AtApSuKuSaRe_20250129_HFScohort2/"  # PC
-# basepath = "Z:/public/projects/AtApSuKuSaRe_20250129_HFScohort2"  # 313
-basepath = "/ceph/mrsic_flogel/public/projects/AtApSuKuSaRe_20250129_HFScohort2"  # HPC
+if Path("/ceph").exists():
+    ROOT = "/ceph/mrsic_flogel/public/projects" # cluster
+elif Path("/Volumes/mrsic_flogel").exists():
+    ROOT = "/Volumes/mrsic_flogel/public/projects" # Mac OS
+else:
+    ROOT = "Y:/public/projects" # Win OS - change drive letter if needed
+basepath = f"{ROOT}/AtApSuKuSaRe_20250129_HFScohort2"  
 animal =  args.animal 
 session = args.session 
 tiff_path = 'funcimg/Session'
 
 data_path = os.path.join(basepath, animal, session, tiff_path)
 
-# Settings 
-ops = suite2p.default_ops()
+## Settings 
+ops = suite2p.default_settings() # default_settings instead of default_ops for new suite2p version
+db = suite2p.default_db()
 
+## General
 ops['tau'] = 0.4
 ops['fs'] = 45
-ops['nchannels'] = 1
-ops['nonrigid'] = False
-ops['smooth_sigma'] = 1.15 # adjust for low SNR (default --1.15)
-ops['smooth_sigma_time'] = 1 # adjust for low SNR (default --1)
-ops['nimg_init'] = 1000 # adjust for low SNR (default --1000)
-ops['two_step_registration'] = False # (default --False)
-ops['keep_movie_raw'] = False # (default --False)
-ops['batch_size'] = 500
-ops['roidetect'] = True
-ops['sparse_mode'] = True
-ops['connected'] = True
-ops['anatomical_only'] = 2
-ops['pretrained_model'] = 'cyto2'
-ops['data_path'] = [data_path]
-ops['save_path'] = [data_path]
+
+## Run control
+ops['run']['do_detection'] = True
+
+## Registration
+ops['registration']['nonrigid'] = False
+ops['registration']['smooth_sigma'] = 1.15
+ops['registration']['smooth_sigma_time'] = 1
+ops['registration']['nimg_init'] = 1000
+ops['registration']['two_step_registration'] = False
+ops['registration']['batch_size'] = 500  # this is reg batch_size, separate from extraction
+
+## Detection (native sparsery, no Cellpose)
+ops['detection']['algorithm'] = 'sparsery'  # default
+ops['detection']['sparsery_settings']['spatial_scale'] = 0
+
+## Anatomical ROI Detection (suite2p with Cellpose) 
+# ops['detection']['algorithm'] = 'cellpose'
+# ops['detection']['cellpose_settings']['cellpose_model'] = 'cyto2'  # or 'cyto3', 'cpsam'
+# ops['diameter'] = [0., 0.]  # 0 = auto-estimate cell size
+
+## db
+db['data_path'] = [data_path]
+db['save_path0'] = data_path  
+db['nchannels'] = 1
+db['keep_movie_raw'] = False
 
 # Run suite2p 
-output_ops = suite2p.run_s2p(ops=ops)
+output_ops = suite2p.run_s2p(settings=ops, db=db)
 
 
